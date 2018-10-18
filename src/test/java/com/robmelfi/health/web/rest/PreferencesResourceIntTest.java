22fbed5619c2b39d5b0f69443caf3b3e8addaf4e
robmelfi <melfiroberto@gmail.com>
2018-10-18 21:37:49 +0200

    Preferences - Back End - [FIX Test] Added logic so non-admin users only see their own data

--- src/test/java/com/robmelfi/health/web/rest/PreferencesResourceIntTest.java
+++ src/test/java/com/robmelfi/health/web/rest/PreferencesResourceIntTest.java
@@ -132,9 +132,16 @@ public class PreferencesResourceIntTest {
     public void createPreferences() throws Exception {
         int databaseSizeBeforeCreate = preferencesRepository.findAll().size();
 
+        // Create security-aware mockMvc
+        restPreferencesMockMvc = MockMvcBuilders
+            .webAppContextSetup(context)
+            .apply(springSecurity())
+            .build();
+
         // Create the Preferences
         PreferencesDTO preferencesDTO = preferencesMapper.toDto(preferences);
         restPreferencesMockMvc.perform(post("/api/preferences")
+            .with(user("user"))
             .contentType(TestUtil.APPLICATION_JSON_UTF8)
             .content(TestUtil.convertObjectToJsonBytes(preferencesDTO)))
             .andExpect(status().isCreated());
@@ -309,6 +316,12 @@ public class PreferencesResourceIntTest {
 
         int databaseSizeBeforeUpdate = preferencesRepository.findAll().size();
 
+        // Create security-aware mockMvc
+        restPreferencesMockMvc = MockMvcBuilders
+            .webAppContextSetup(context)
+            .apply(springSecurity())
+            .build();
+
         // Update the preferences
         Preferences updatedPreferences = preferencesRepository.findById(preferences.getId()).get();
         // Disconnect from session so that the updates on updatedPreferences are not directly saved in db
@@ -319,6 +332,7 @@ public class PreferencesResourceIntTest {
         PreferencesDTO preferencesDTO = preferencesMapper.toDto(updatedPreferences);
 
         restPreferencesMockMvc.perform(put("/api/preferences")
+            .with(user("user"))
             .contentType(TestUtil.APPLICATION_JSON_UTF8)
             .content(TestUtil.convertObjectToJsonBytes(preferencesDTO)))
             .andExpect(status().isOk());
